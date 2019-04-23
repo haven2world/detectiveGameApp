ea671e1aa39c044035185b57adbe75c9048c466f
haven2world <415122975@qq.com>
2019-04-23 17:50:26 +0800

    [feat]: 修改部署配置

--- frontend/.umirc.prod.js
+++ frontend/.umirc.prod.js
@@ -2,6 +2,8 @@ var path = require('path');
 
 // ref: https://umijs.org/config/
 export default {
+  base:'/detective',
+  publicPath:'/detective/',
   define:{
     'process.env.NODE_ENV':'production'
   },
